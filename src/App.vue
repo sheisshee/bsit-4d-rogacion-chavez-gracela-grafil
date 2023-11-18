300dd833afcd8d96814587e25f920e0d47357020
Sherrelyn <chavezsherrelyn13@gmail.com>
2023-11-18 15:29:44 +0800

    tried importing loop

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import BaseButton from './components/BaseButton.vue';
+  import BaseList from './components/BaseList.vue';
 </script>
 
 <template>
   <BaseButton></BaseButton>
+  <BaseList></BaseList>
 </template>
