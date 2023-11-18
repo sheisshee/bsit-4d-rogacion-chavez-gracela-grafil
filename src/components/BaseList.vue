6be1003d2c02693b6bd324096a929579a4995b9d
Sherrelyn <chavezsherrelyn13@gmail.com>
2023-11-18 15:27:02 +0800

    tried loop

---
+++ src/components/BaseList.vue
@@ -0,0 +1,11 @@
+<script setup>
+let numbers = [0,1,2,3,4]
+</script>
+
+<template>
+    <ul>
+        <li v-for="number in numbers">
+            {{ number }} </li>
+        
+    </ul>
+</template>
