f66a8ae8642ed3ffe6284337cb42fd895ed6a042
sheisshee <chavezsherrelyn13@gmail.com>
2023-11-18 16:44:35 +0800

    created simple chat message for baseloop file

--- src/components/BaseLoop.vue
+++ src/components/BaseLoop.vue
@@ -1,24 +1,36 @@
-
 <script setup>
 import { ref } from 'vue';
 
-// Data property
-const colors = ref(['#FDE5D4', '#D6CC99', '#445D48', '#001524', '#22668D', '#EDB7ED', '#82A0D8', '#8DDFCB', '#ECEE81', '#F9DEC9', '#D80032', '#F78CA2']);
-const newColor = ref('');
+// Initial chat messages
+let messages = ref([
+  { text: 'Hello', sender: 'user' },
+  { text: 'Hi there!', sender: 'bot' },
+  { text: 'How are you?', sender: 'bot' },
+]);
 
-// Method to add a new color
-const addColor = () => {
-  if (newColor.value) {
-    colors.value.push(newColor.value);
-    newColor.value = '';
-  }
-};
-
-// Method to remove a color
-const removeColor = (index) => {
-  colors.value.splice(index, 1);
+// Method to add a new message
+const addMessage = (text, sender) => {
+  messages.value.push({ text, sender });
 };
 </script>
 
 <template>
+  <div>
+    <ul>
+      <li v-for="message in messages" :key="message.text" :class="{ 'user-message': message.sender === 'user', 'bot-message': message.sender === 'bot' }">
+        {{ message.text }}
+      </li>
+    </ul>
+    <button @click="addMessage('Hello', 'user')">Send Message</button>
+  </div>
 </template>
+
+<style scoped>
+.user-message {
+  color: blue;
+}
+
+.bot-message {
+  color: green;
+}
+</style>
